a697f192acce15a425201f448b03377c0fb04410
Adaihao <gyl_adaihao@163.com>
2015-11-27 14:05:15 +0800

    :tada: Added .gitattributes & .gitignore files

---
+++ simpleFactoryPattern/src/main/java/com/simpleFactory/Client.java
@@ -0,0 +1,14 @@
+package com.simpleFactory;
+
+public class Client {
+    public static void main(String args[]) {
+        try {
+            TV tv;
+            String brandName = XMLUtilTV.getBrandName();
+            tv = TVFactory.produceTV(brandName);
+            tv.play();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
