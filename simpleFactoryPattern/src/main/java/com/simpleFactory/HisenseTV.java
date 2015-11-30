d8f431331f43634116c89f0ffcea081549b93790
Adaihao <gyl_adaihao@163.com>
2015-11-30 16:50:11 +0800

    简单工厂模式
    简单工厂模式

--- simpleFactoryPattern/src/main/java/com/simpleFactory/HisenseTV.java
+++ simpleFactoryPattern/src/main/java/com/simpleFactory/HisenseTV.java
@@ -2,6 +2,6 @@ package com.simpleFactory;
 
 public class HisenseTV implements TV {
     public void play() {
-        System.out.println("海信电视机播放中......");
+        System.out.println("海信电视机播放中........");
     }
 }
